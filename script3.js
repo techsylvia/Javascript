eb517c53448c967bd2b39ab97f801f3a64ca5ea6
Sylvia <sylvia@donchev.is>
2022-10-12 20:58:33 +0200

    Boolean | let | if | else | conosole.log

--- script3.js
+++ script3.js
@@ -2,5 +2,26 @@
 console.log(Boolean(0)); // false
 console.log(Boolean(undefined)); // false
 console.log(Boolean("Jonas")); // true
-console.log(Boolean({})); // true
-console.log(Boolean("")); // false
+console.log(Boolean({})); // true any string that is not an empty string has a value of true
+console.log(Boolean("")); // false because it's empty string
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height; // we did not sign it any value so it's undefined
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+
+let theHeight = 1;
+if (theHeight) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
