c101a3c862be36eb00df36d9076c779bbbf3056b
Sylvia <sylvia@donchev.is>
2022-10-21 10:39:30 +0200

    challenges

---
+++ script8.js
@@ -0,0 +1,17 @@
+//const age = 23;
+
+//age >= 18;
+//console.log("I like to drink wine 🍷");
+//console.log("I like to drink water 💧");
+
+// operator always produces value and is an expression
+// if it has a value it can assign that value to a variable
+
+//document.querySelectorAll("input");
+//let personalDetails = document.querySelectorAll(".personal-details");
+//personalDetails.querySelectorAll("input");
+
+let mainTitle = document.querySelector("h1");
+console.log(mainTitle.innerText); // -> <h1></h1>
+mainTitle.innerText = "THIS IS EPIC";
+console.log(mainTitle.innerText); // -> <h1>THIS IS EPIC</h1>
---
+++ script9.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evens = numbers.filter((num) => num % 2 === 0);
+
+// [10, 8, 18, 6]
+console.log(evens);
