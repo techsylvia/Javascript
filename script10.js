e5ca7f233d983ea861a622d443d4f8d54addfead
Sylvia <sylvia@donchev.is>
2022-10-23 12:01:53 +0200

    code

--- script10.js
+++ script10.js
@@ -10,7 +10,7 @@
 //(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
 //316.25”
 
-const bill = 275;
+const bill = 430;
 // bill must be less or equal 300 & at the same time
 // bill must be greater or equal than 50
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
